24f71e6f91b5fd0987219a60b711cb4f433316b1
aereal <aereal@aereal.org>
2022-02-28 23:44:24 +0900

    docs: move example to cmd/injecuet

---
+++ cmd/injecuet/example_test.go
@@ -0,0 +1,19 @@
+package main_test
+
+import (
+	"os"
+	"os/exec"
+)
+
+func Example_tfstate() {
+	cmd := exec.Command("go", "run", "github.com/aereal/injecuet/cmd/injecuet", "../../testdata/ok_tfstate.cue")
+	cmd.Stdout = os.Stdout
+	if err := cmd.Run(); err != nil {
+		panic(err)
+	}
+	// Output: {
+	// 	@inject(tfstate,stateURL=./terraform/ok/terraform.tfstate)
+	// 	name: "aereal" @inject(tfstate,name=output.user.name)
+	// 	age:  17       @inject(tfstate,name=output.user.age)
+	// }
+}
