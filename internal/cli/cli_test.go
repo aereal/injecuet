cee1c19f8cac83d91e8105456188f2eb7221c533
aereal <aereal@aereal.org>
2022-03-01 21:33:00 +0900

    test: explicitly pass test case name

--- internal/cli/cli_test.go
+++ internal/cli/cli_test.go
@@ -2,10 +2,8 @@ package cli
 
 import (
 	"bytes"
-	"fmt"
 	"io/ioutil"
 	"os"
-	"strings"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -17,21 +15,22 @@ func TestRun(t *testing.T) {
 		content string
 	}
 	type testCase struct {
+		name   string
 		argv   []string
 		out    *output
 		stderr string
 		status int
 	}
 	cases := []testCase{
-		{[]string{"injecuet", "-output", "../../testdata/ok.out.cue", "../../testdata/ok.cue"}, &output{path: "../../testdata/ok.out.cue", content: "{\n\tname: string @inject(env,name=X_NAME)\n}\n"}, "", 0},
-		{[]string{"injecuet"}, nil, "input file must be given\n", 1},
-		{[]string{"injecuet", "../../testdata/ok.cue", "../../testdata/not_found.cue"}, nil, "input file must be given\n", 1},
-		{[]string{"injecuet", "missing_file.cue"}, nil, "failed to inject values to file missing_file.cue: cannot parse file(missing_file.cue): open missing_file.cue: no such file or directory\n", 1},
-		{[]string{"injecuet", "-pattern", "[", "-output", "../../testdata/ok.out.cue", "../../testdata/ok.cue"}, nil, "cannot parse pattern: error parsing regexp: missing closing ]: `[`\n", 1},
-		{[]string{"injecuet", "-pattern", "AGE$", "-output", "../../testdata/partial.out.cue", "../../testdata/partial.cue"}, &output{path: "../../testdata/partial.out.cue", content: "{\n\tname: string @inject(env,name=X_NAME)\n\tage:  string @inject(env,name=X_AGE)\n}\n"}, "", 0},
+		{"ok", []string{"injecuet", "-output", "../../testdata/ok.out.cue", "../../testdata/ok.cue"}, &output{path: "../../testdata/ok.out.cue", content: "{\n\tname: string @inject(env,name=X_NAME)\n}\n"}, "", 0},
+		{"no input file", []string{"injecuet"}, nil, "input file must be given\n", 1},
+		{"refer missing environment variable", []string{"injecuet", "../../testdata/ok.cue", "../../testdata/not_found.cue"}, nil, "input file must be given\n", 1},
+		{"input file not found", []string{"injecuet", "missing_file.cue"}, nil, "failed to inject values to file missing_file.cue: cannot parse file(missing_file.cue): open missing_file.cue: no such file or directory\n", 1},
+		{"corrupt pattern", []string{"injecuet", "-pattern", "[", "-output", "../../testdata/ok.out.cue", "../../testdata/ok.cue"}, nil, "cannot parse pattern: error parsing regexp: missing closing ]: `[`\n", 1},
+		{"ok; with pattern", []string{"injecuet", "-pattern", "AGE$", "-output", "../../testdata/partial.out.cue", "../../testdata/partial.cue"}, &output{path: "../../testdata/partial.out.cue", content: "{\n\tname: string @inject(env,name=X_NAME)\n\tage:  string @inject(env,name=X_AGE)\n}\n"}, "", 0},
 	}
 	for _, tc := range cases {
-		t.Run(fmt.Sprintf("args=%s", strings.Join(tc.argv, " ")), func(t *testing.T) {
+		t.Run(tc.name, func(t *testing.T) {
 			errOut := new(bytes.Buffer)
 			app := &App{errOut: errOut}
 			got := app.Run(tc.argv)
