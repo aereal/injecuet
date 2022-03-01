5c278c345d88d84f4a32888b9e4307b72a207589
aereal <aereal@aereal.org>
2022-03-01 22:12:31 +0900

    feat: add -log-level option

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -9,6 +9,7 @@ import (
 
 	"cuelang.org/go/cue/format"
 	"github.com/aereal/injecuet"
+	"github.com/rs/zerolog"
 )
 
 var version string
@@ -33,10 +34,12 @@ func (a *App) Run(argv []string) int {
 		outPath     string
 		showVersion bool
 		pattern     string
+		level       = logLevel{zerolog.InfoLevel}
 	)
 	fs.StringVar(&outPath, "output", "", "output file path. default is stdout")
 	fs.BoolVar(&showVersion, "version", false, "show version")
 	fs.StringVar(&pattern, "pattern", "", "regular expression of environment variables' names to consume; the pattern must be valid as Go's regexp")
+	fs.Var(&level, "log-level", "log level")
 	fs.SetOutput(a.errOut)
 	switch err := fs.Parse(argv[1:]); err {
 	case flag.ErrHelp:
@@ -57,9 +60,10 @@ func (a *App) Run(argv []string) int {
 	}
 
 	opts := runOptions{
-		srcPath: fs.Arg(0),
-		outPath: outPath,
-		pattern: pattern,
+		srcPath:  fs.Arg(0),
+		outPath:  outPath,
+		pattern:  pattern,
+		logLevel: level.Level,
 	}
 	if err := a.runMain(opts); err != nil {
 		fmt.Fprintf(a.errOut, "%v\n", err)
@@ -80,7 +84,7 @@ func (a *App) runMain(opts runOptions) error {
 	}
 	defer close()
 
-	injector := injecuet.NewInjector(injecuet.WithEnvironmentVariables(match), injecuet.WithTFState())
+	injector := injecuet.NewInjector(injecuet.WithEnvironmentVariables(match), injecuet.WithTFState(), injecuet.WithLogLevel(opts.logLevel))
 	v, err := injector.Inject(opts.srcPath)
 	if err != nil {
 		return fmt.Errorf("failed to inject values to file %s: %w", opts.srcPath, err)
@@ -95,9 +99,10 @@ func (a *App) runMain(opts runOptions) error {
 }
 
 type runOptions struct {
-	srcPath string
-	outPath string
-	pattern string
+	srcPath  string
+	outPath  string
+	pattern  string
+	logLevel zerolog.Level
 }
 
 func (o runOptions) buildMatchFunction() (func(string) bool, error) {
@@ -121,4 +126,21 @@ func (o runOptions) openOutput() (io.Writer, func(), error) {
 		return nil, func() {}, fmt.Errorf("cannot open file %s: %w", o.outPath, err)
 	}
 	return f, func() { f.Close() }, nil
+}
+
+type logLevel struct {
+	zerolog.Level
+}
+
+func (l logLevel) String() string {
+	return l.Level.String()
+}
+
+func (l *logLevel) Set(raw string) error {
+	parsed, err := zerolog.ParseLevel(raw)
+	if err != nil {
+		return err
+	}
+	l.Level = parsed
+	return nil
 }
--- internal/cli/cli_test.go
+++ internal/cli/cli_test.go
@@ -22,12 +22,14 @@ func TestRun(t *testing.T) {
 		status int
 	}
 	cases := []testCase{
-		{"ok", []string{"injecuet", "-output", "../../testdata/ok.out.cue", "../../testdata/ok.cue"}, &output{path: "../../testdata/ok.out.cue", content: "{\n\tname: string @inject(env,name=X_NAME)\n}\n"}, "", 0},
+		{"ok", []string{"injecuet", "-output", "../../testdata/ok.out.cue", "../../testdata/ok.cue"}, &output{path: "../../testdata/ok.out.cue", content: "{\n\tname: string @inject(env,name=X_NAME)\n}\n"}, "{\"level\":\"warn\",\"path\":\"name\",\"key\":\"X_NAME\",\"error\":\"value not found: X_NAME\",\"message\":\"failed to fill value\"}\n", 0},
 		{"no input file", []string{"injecuet"}, nil, "input file must be given\n", 1},
 		{"refer missing environment variable", []string{"injecuet", "../../testdata/ok.cue", "../../testdata/not_found.cue"}, nil, "input file must be given\n", 1},
 		{"input file not found", []string{"injecuet", "missing_file.cue"}, nil, "failed to inject values to file missing_file.cue: cannot parse file(missing_file.cue): open missing_file.cue: no such file or directory\n", 1},
 		{"corrupt pattern", []string{"injecuet", "-pattern", "[", "-output", "../../testdata/ok.out.cue", "../../testdata/ok.cue"}, nil, "cannot parse pattern: error parsing regexp: missing closing ]: `[`\n", 1},
-		{"ok; with pattern", []string{"injecuet", "-pattern", "AGE$", "-output", "../../testdata/partial.out.cue", "../../testdata/partial.cue"}, &output{path: "../../testdata/partial.out.cue", content: "{\n\tname: string @inject(env,name=X_NAME)\n\tage:  string @inject(env,name=X_AGE)\n}\n"}, "", 0},
+		{"ok; with pattern", []string{"injecuet", "-pattern", "AGE$", "-output", "../../testdata/partial.out.cue", "../../testdata/partial.cue"}, &output{path: "../../testdata/partial.out.cue", content: "{\n\tname: string @inject(env,name=X_NAME)\n\tage:  string @inject(env,name=X_AGE)\n}\n"}, "{\"level\":\"warn\",\"path\":\"name\",\"key\":\"X_NAME\",\"error\":\"value not found: X_NAME\",\"message\":\"failed to fill value\"}\n{\"level\":\"warn\",\"path\":\"age\",\"key\":\"X_AGE\",\"error\":\"value not found: X_AGE\",\"message\":\"failed to fill value\"}\n", 0},
+		{"specify log level", []string{"injecuet", "-log-level", "debug", "-output", "../../testdata/ok.out.cue", "../../testdata/ok.cue"}, &output{path: "../../testdata/ok.out.cue", content: "{\n\tname: string @inject(env,name=X_NAME)\n}\n"}, "{\"level\":\"warn\",\"path\":\"name\",\"key\":\"X_NAME\",\"error\":\"value not found: X_NAME\",\"message\":\"failed to fill value\"}\n", 0},
+		{"pass invalid log level", []string{"injecuet", "-log-level", "unknown", "-output", "../../testdata/ok.out.cue", "../../testdata/ok.cue"}, nil, "invalid value \"unknown\" for flag -log-level: Unknown Level String: 'unknown', defaulting to NoLevel\nUsage of injecuet:\n  -log-level value\n    \tlog level (default info)\n  -output string\n    \toutput file path. default is stdout\n  -pattern string\n    \tregular expression of environment variables' names to consume; the pattern must be valid as Go's regexp\n  -version\n    \tshow version\n", 1},
 	}
 	for _, tc := range cases {
 		t.Run(tc.name, func(t *testing.T) {
